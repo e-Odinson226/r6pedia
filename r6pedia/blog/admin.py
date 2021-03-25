e4b20d73bf534e80ce033c79267590d5082dd2b1
erfan <erfan.yekezare@gmail.com>
2021-03-25 17:47:11 +0430

    Major configurations on [blog/admin.py]. Minor edit on [Post] model.

--- r6pedia/blog/admin.py
+++ r6pedia/blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("subject", "content", "published", "status")
+    list_filter = ("status", "published")
+    search_fields = ("subject", "content")
+    prepopulated_fields = {"slug":("subject",)}
+    ordering = ["-status", "-published"] 
+admin.site.register(Post, PostAdmin)
--- r6pedia/blog/models.py
+++ r6pedia/blog/models.py
@@ -7,14 +7,14 @@ class Post(models.Model):
         ('p', "published"),
         ('d', "drafted"  ),
     ]
-    subject         = models.CharField(max_length=50, verbose_name="تیتر")
-    slug            = models.SlugField(unique=True, max_length=40)
-    content         = models.TextField(max_length=100000, verbose_name="مطالب")
-    img             = models.ImageField(upload_to="images", verbose_name="تصویر")
-    write_date      = models.DateTimeField(auto_now_add=True)
-    publish_date    = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
-    update_date     = models.DateTimeField(auto_now=True)
-    publish_status  = models.CharField(max_length=1, choices=publish_stats_choices, verbose_name="وضعیت")
+    subject     = models.CharField(max_length=50, verbose_name="تیتر")
+    slug        = models.SlugField(unique=True, max_length=40)
+    content     = models.TextField(max_length=100000, verbose_name="مطالب")
+    img         = models.ImageField(upload_to="images", verbose_name="تصویر")
+    wrote       = models.DateTimeField(auto_now_add=True)
+    published   = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
+    updated     = models.DateTimeField(auto_now=True)
+    status      = models.CharField(max_length=1, choices=publish_stats_choices, verbose_name="وضعیت")
 
     def __str__(self):
         return self.subject
