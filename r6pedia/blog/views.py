883d4121f096e2abff90dd6193e951c0a4b59c44
erfan <erfan.yekezare@gmail.com>
2021-03-25 19:18:21 +0430

    Create [homepage] view in [blog/views.py]. Add a temporary html file for it.

--- r6pedia/blog/views.py
+++ r6pedia/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def homepage(request):
+    posts= Post.objects.filter(status="p").order_by("-published")
+    context = {
+        "posts" : posts
+    }
+    return render(request, "blog/homepage.html", context)
