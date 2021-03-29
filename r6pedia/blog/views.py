6857497cbd5336271e7fd5bcf9145d5954c59335
erfan <erfan.yekezare@gmail.com>
2021-03-29 16:42:18 +0430

    Create [magazin] & [post_detail] views.

--- r6pedia/blog/views.py
+++ r6pedia/blog/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def homepage(request):
+    return render(request, "blog/homepage.html")
+
+def magazin(request):
     posts= Post.objects.filter(status="p").order_by("-published")
     context = {
         "posts" : posts
     }
-    return render(request, "blog/homepage.html", context)
+    return render(request, "blog/magazin.html", context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {
+        "post" : post
+    }
+    return render(request, "blog/single-post.html", context)
