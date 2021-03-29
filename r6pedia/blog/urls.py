c8a7a1d7b84a4fe6f278f2bc0c89a34514bc92b1
erfan <erfan.yekezare@gmail.com>
2021-03-29 16:41:23 +0430

    Add [mag] & [single-post] views.

--- r6pedia/blog/urls.py
+++ r6pedia/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ( homepage, )
+from .views import( homepage,
+                    magazin,
+                    post_detail )
 
 app_name = "blog"
 urlpatterns = [
-    path('', homepage, name="homepage")
+    path('', homepage, name="homepage"),
+    path('mag/', magazin, name="mag"),
+    path('mag/<slug:slug>', post_detail, name="single-post"),
 ]
