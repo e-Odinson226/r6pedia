cfc06cae735f64b6bc9eb8bea284a43bed60c4cf
erfan <erfan.yekezare@gmail.com>
2021-03-30 19:29:36 +0430

    Add [Category] model to [Admin] panel. Also migrate.

--- r6pedia/blog/admin.py
+++ r6pedia/blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "rank")
+    list_filter = (["rank"])
+    search_fields = (["title"])
+    prepopulated_fields = {"slug":("title",)}
+    ordering = ["-rank"] 
+admin.site.register(Category, CategoryAdmin)
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("subject", "content", "j_published", "status")
