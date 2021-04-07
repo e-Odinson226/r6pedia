1cbf3efc9144097c8d90dd7ff1e5cd176e05469d
erfan <erfan.yekezare@gmail.com>
2021-04-07 20:23:25 +0430

    Add category's status as a list-filter in admin panels.

--- r6pedia/blog/admin.py
+++ r6pedia/blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post, Category
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "rank")
+    list_display = ("title", "slug", "rank", "status")
     list_filter = (["rank"])
     search_fields = (["title"])
     prepopulated_fields = {"slug":("title",)}
