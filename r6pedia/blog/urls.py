aeb5d8fea847658e5812757812436d762d699ae0
erfan <erfan.yekezare@gmail.com>
2021-05-04 17:11:27 +0430

    1. Reconfig [blog.urls.py] and add [Blog] as a class-base view instead of [blog_main]. 2.  Reconfig [blog/blog_main.html] to access posts and be able to paginate.

--- r6pedia/blog/urls.py
+++ r6pedia/blog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import( Homepage,
-                    blog_main,
+                    Blog,
                     post_detail,
                     category )
 
 app_name = "blog"
 urlpatterns = [
-    path('', Homepage.as_view(), name="homepage"),
-    path('blog/', blog_main, name="blog"),
+    path('home/', Homepage.as_view(), name="homepage"),
+    path('', Blog.as_view(), name="blog"),
     path('blog/<slug:slug>', post_detail, name="single-post"),
     path('category/<slug:slug>', category, name="category"),
 ]
